a27c9fd1f02dc4712dff1150642e5f6dd217323a
Gabriel Gheorghe <gabriel.gheorghe.it@gmail.com>
2023-10-03 22:14:29 +0300

    Add single support to queries

--- crates/wings_ui/src/queries/color.rs
+++ crates/wings_ui/src/queries/color.rs
@@ -4,23 +4,45 @@ use wings_utils::color::get_random_color;
 
 #[derive(SystemParam)]
 pub struct UiColorQuery<'w, 's, T: Component> (
-    Query<'w, 's, (&'static mut BackgroundColor, With<T>)>,
+    Query<'w, 's,
+        (
+            Entity,
+            &'static mut BackgroundColor,
+            With<T>,
+        ),
+    >,
 );
 
 impl <'w, 's, T: Component> UiColorQuery<'w, 's, T> {
     pub fn set(&mut self, color: Color) {
-        self.0.for_each_mut(|(mut c_color, _)| {
+        self.0.for_each_mut(|(_, mut c_color, _)| {
             c_color.0 = color;
         });
     }
 
     pub fn set_random(&mut self) {
-        self.0.for_each_mut(|(mut c_color, _)| {
+        self.0.for_each_mut(|(_, mut c_color, _)| {
             c_color.0 = get_random_color();
         });
     }
 
     pub fn set_random_equally(&mut self) {
         self.set(get_random_color());
+    }
+
+    pub fn set_single(&mut self, target: Entity, color: Color) {
+        self.0.for_each_mut(|(entity, mut c_color, _)| {
+            if entity == target {
+                c_color.0 = color;
+            }
+        });
+    }
+
+    pub fn set_random_single(&mut self, target: Entity) {
+        self.0.for_each_mut(|(entity, mut c_color, _)| {
+            if entity == target {
+                c_color.0 = get_random_color();
+            }
+        });
     }
 }
--- crates/wings_ui/src/queries/visibility.rs
+++ crates/wings_ui/src/queries/visibility.rs
@@ -7,20 +7,31 @@ use crate::utils::{get_computed_display, get_computed_visibility};
 pub struct UiVisibilityQuery<'w, 's, T: Component>(
     Query<'w, 's,
         (
+            Entity,
             &'static mut Style,
             &'static mut Visibility,
             &'static mut UiVisibility,
             With<T>,
-        )
+        ),
     >,
 );
 
 impl <'w, 's, T: Component> UiVisibilityQuery<'w, 's, T> {
     pub fn set(&mut self, visibility: UiVisibility) {
-        self.0.for_each_mut(|(mut c_style, mut c_visibility, mut ui_visibility, _)| {
+        self.0.for_each_mut(|(_, mut c_style, mut c_visibility, mut ui_visibility, _)| {
             c_style.display = get_computed_display(&visibility);
             *c_visibility = get_computed_visibility(&visibility);
             *ui_visibility = visibility;
+        });
+    }
+
+    pub fn set_single(&mut self, target: Entity, visibility: UiVisibility) {
+        self.0.for_each_mut(|(entity, mut c_style, mut c_visibility, mut ui_visibility, _)| {
+            if entity == target {
+                c_style.display = get_computed_display(&visibility);
+                *c_visibility = get_computed_visibility(&visibility);
+                *ui_visibility = visibility;
+            }
         });
     }
 }
