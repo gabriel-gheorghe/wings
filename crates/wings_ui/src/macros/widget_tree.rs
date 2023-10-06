a3da6a008a35e581e97bb090991bdf31807fbf99
Gabriel Gheorghe <gabriel.gheorghe.it@gmail.com>
2023-10-06 04:24:31 +0300

    Update widget_tree.rs

--- crates/wings_ui/src/macros/widget_tree.rs
+++ crates/wings_ui/src/macros/widget_tree.rs
@@ -3,20 +3,38 @@
 #[macro_export]
 macro_rules! define_ui_types {
     () => {
+        // WIDGETS
         type Scaffold = UiScaffoldBundle;
         type Container = UiContainerBundle;
         type SizedBox = UiSizedBoxBundle;
-        type Padding = UiPaddingBundle;
         type Align = UiAlignBundle;
         type Center = UiCenterBundle;
+        type Button = UiButtonBundle;
         type Column = UiColumnBundle;
         type Row = UiRowBundle;
+        type ConstrainedWidth = UiConstrainedWidthBundle;
+        type ConstrainedHeight = UiConstrainedHeightBundle;
+        type HorizontalDivider = UiHorizontalDividerBundle;
+        type VerticalDivider = UiVerticalDividerBundle;
+        type Visibility = UiVisibilityBundle;
+        type LayoutVisibility = UiLayoutVisibilityBundle;
+        type Padding = UiPaddingBundle;
 
+        // CLASSES
+        type ViewPadding = UiViewPadding;
         type EdgeInsets = UiEdgeInsets;
         type Alignment = UiAlignment;
+
+        // ENUMS
         type MainAxisSize = UiMainAxisSize;
         type MainAxisAlignment = UiMainAxisAlignment;
         type CrossAxisAlignment = UiCrossAxisAlignment;
+
+        // EVENTS
+        type PointerClick = UiPointerClick;
+
+        // TAGS
+        type Collapsible = UiCollapsible;
     }
 }
 
